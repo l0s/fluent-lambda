0f225285de27dca552c590d98e67ef0a2ed24517
Carlos Macasaet <github@carlos-m.net>
2014-02-27 23:42:34 -0800

    Initial commit.

---
+++ src/main/java/com/macasaet/lambda/fluent/FluentFunctions.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright 2014 Carlos Macasaet
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.macasaet.lambda.fluent;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.annotation.Nullable;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
+/**
+ * 
+ * <p>Copyright &copy; 2014 Carlos Macasaet.</p>
+ *
+ * @author Carlos Macasaet
+ */
+public class FluentFunctions {
+
+    private static final Logger logger = LoggerFactory.getLogger(FluentFunctions.class);
+    private static final ThreadLocal<Method> methodHolder = new ThreadLocal<Method>();
+
+    @SuppressWarnings("unchecked")
+    public static <T> T ofClass(final Class<T> c) {
+        final MethodInterceptor interceptor = new MethodInterceptor() {
+            public Object intercept(final Object obj, final Method method, final Object[] args, final MethodProxy proxy) {
+                final Class<?> returnType = method.getReturnType();
+
+                checkState(methodHolder.get() == null,
+                        "Improper usage: multiple calls to forMethod for a single call to ofClass.");
+                checkArgument(c.isInstance(obj), "Expected object of type: %s", c);                
+                checkArgument(returnType != void.class, "Method must return an Object: %s", method);
+                checkArgument(args == null || args.length == 0, "Method must have no parameters: %s", method);
+
+                methodHolder.set(method);
+
+                if (!returnType.isPrimitive()) {
+                    return null;
+                } else if (boolean.class.equals(returnType)) {
+                    return false;
+                }
+                return 0;
+            }
+        };
+        final Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(c);
+        enhancer.setCallback(interceptor);
+        return (T) enhancer.create();
+    }
+
+    public static <X, Y> Function<? super X, ? extends Y> forMethod(final Y invocation) {
+        final Method method = methodHolder.get();
+        methodHolder.remove();
+        checkState(method != null, "Improper usage: ofClass has not been called");
+        return new Function<X, Y>() {
+            @SuppressWarnings("unchecked")
+            public Y apply(final X x) {
+                try {
+                    return (Y) method.invoke(x, new Object[0]);
+                } catch (final IllegalAccessException e) {
+                    logger.error(e.getMessage(), e);
+                    throw new RuntimeException(e.getMessage(), e);
+                } catch (InvocationTargetException e) {
+                    logger.error(e.getMessage(), e);
+                    final Throwable cause = e.getCause();
+                    throw new RuntimeException(cause.getMessage(), cause);
+                }
+            }
+        };
+    }
+
+    public static <X> Predicate<? super X> forMethod(final boolean invocation) {
+        final Method method = methodHolder.get();
+        methodHolder.remove();
+        checkState(method != null, "Improper usage: ofClass has not been called");
+        checkState(boolean.class.equals( method.getReturnType() ), "Method does not return a boolean.");
+        return new Predicate<X>() {
+
+            public boolean apply(@Nullable X x) {
+                try {
+                    return (boolean) method.invoke(x, new Object[0]);
+                } catch (final IllegalAccessException e) {
+                    logger.error(e.getMessage(), e);
+                    throw new RuntimeException(e.getMessage(), e);
+                } catch (InvocationTargetException e) {
+                    logger.error(e.getMessage(), e);
+                    final Throwable cause = e.getCause();
+                    throw new RuntimeException(cause.getMessage(), cause);
+                }
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/macasaet/lambda/fluent/FluentFunctionsTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright 2014 Carlos Macasaet
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.macasaet.lambda.fluent;
+
+import static com.macasaet.lambda.fluent.FluentFunctions.forMethod;
+import static com.macasaet.lambda.fluent.FluentFunctions.ofClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.apache.commons.math.linear.AnyMatrix;
+import org.apache.commons.math.linear.Array2DRowRealMatrix;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
+/**
+ * Test class for {@link FluentFunctions}.
+ *
+ * <p>Copyright &copy; 2014 Carlos Macasaet.</p>
+ *
+ * @author Carlos Macasaet
+ */
+public class FluentFunctionsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public final void verifyMethodCaptured() {
+        // given
+        final Function<? super Logger, ? extends String> f = forMethod(ofClass(Logger.class).getName());
+        final Logger x = LoggerFactory.getLogger("name");
+
+        // when
+        final String y = f.apply(x);
+
+        // then
+        assertEquals("name", y);
+    }
+
+    @Test
+    public final void verifyMultipleCapturesThrowsException() {
+        // given
+        final Logger stub = ofClass(Logger.class);
+
+        // when
+        try {
+            stub.getName();
+            stub.getName();
+
+            // then
+            fail("Expected RuntimeException.");
+        } catch (final RuntimeException re) {
+        }
+    }
+
+    @Test
+    public final void verifyForMethodWithoutOfClassThrowsException() {
+        // given
+        final Logger logger = LoggerFactory.getLogger(getClass());
+
+        // when
+        try {
+            forMethod(logger.getName());
+
+            // then
+            fail("Expected RuntimeException.");
+        } catch (final RuntimeException re) {
+        }
+    }
+
+    @Test
+    public final void verifyPredicateEvaluatesTrue() {
+        // given
+        final Array2DRowRealMatrix squareMatrix = new Array2DRowRealMatrix(new double[][] { {0, 1}, {2, 3}});
+
+        // when
+        final Predicate<? super AnyMatrix> predicate = forMethod(ofClass(AnyMatrix.class).isSquare());
+
+        // then
+        assertTrue(predicate.apply(squareMatrix));
+    }
+
+//    @Test // FIXME
+    public final void verifyPredicateEvaluatesFalse() {
+        // given
+        final Array2DRowRealMatrix nonSquareMatrix = new Array2DRowRealMatrix(new double[][] { {0, 1, 3}, {4, 5, 6}});
+
+        // when
+        final Predicate<? super AnyMatrix> predicate = forMethod(ofClass(AnyMatrix.class).isSquare());
+
+        // then
+        assertFalse(predicate.apply(nonSquareMatrix));
+    }
+}
